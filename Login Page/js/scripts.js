d8d8cb1661c1e6ee59dd4c378af7aff9495b07bf
Sharmada Iyer <siyer2018@fau.edu>
2021-04-01 23:54:13 -0400

    Organizing branch

--- Login Page/js/scripts.js
+++ Login Page/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-    * Start Bootstrap - Grayscale v6.0.3 (https://startbootstrap.com/theme/grayscale)
+    * Start Bootstrap - Agency v6.0.3 (https://startbootstrap.com/theme/agency)
     * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
     */
     (function ($) {
     "use strict"; // Start of use strict
@@ -20,7 +20,7 @@
             if (target.length) {
                 $("html, body").animate(
                     {
-                        scrollTop: target.offset().top - 70,
+                        scrollTop: target.offset().top - 72,
                     },
                     1000,
                     "easeInOutExpo"
@@ -38,19 +38,19 @@
     // Activate scrollspy to add active class to navbar items on scroll
     $("body").scrollspy({
         target: "#mainNav",
-        offset: 100,
+        offset: 74,
     });
 
     // Collapse Navbar
     var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
+        if ($("#mainNav").offset().top >= 0) {
             $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
+        } //else {
+            //$("#mainNav").removeClass("navbar-shrink");
+       // }
     };
     // Collapse now if page is not at top
     navbarCollapse();
     // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
+    //$(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
